710e69112219615f5ea5fb5453c600cafddc78d8
GREENTECK9810 <vishal.waliyan47@gmail.com>
2022-11-08 23:12:21 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,56 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Routes, Route, Link } from "react-router-dom";
+import { Layout, Typography, Space } from "antd";
 
-function App() {
+import "antd/dist/antd.css";
+import "./App.css";
+import {
+  Navbar,
+  Homepage,
+  Cryptocurrencies,
+  CryptoDetails,
+  Exchanges,
+} from "./components";
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <div className="navbar">
+        <Navbar />
+      </div>
+      <div className="main">
+        <Layout>
+          <div className="routes">
+            <Routes>
+              <Route path="/" element=<Homepage /> />
+              <Route exact path="/exchanges" element=<Exchanges /> />
+              <Route
+                exact
+                path="/cryptocurrencies"
+                element=<Cryptocurrencies />
+              />
+              <Route exact path="/crypto/:coinId" element=<CryptoDetails /> />
+            </Routes>
+          </div>
+        </Layout>
+
+        <div className="footer">
+          <Typography.Title
+            level={5}
+            style={{ color: "white", textAlign: "center" }}
+          >
+            Cryptopia <br />
+            All rights reserved
+          </Typography.Title>
+          <Space>
+            <Link to="/">Home</Link>
+            <Link to="/cryptocurrencies">Cryptocurrencies</Link>
+            <Link to="/exchanges">Exchanges</Link>
+          </Space>
+        </div>
+      </div>
     </div>
   );
-}
+};
 
 export default App;
